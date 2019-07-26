675ff240e744b2e2fa2d54d851c19fa0f46664d4
AtkLordOverAll <darkninjabuni@gmail.com>
2019-07-26 15:03:25 +0100

    Create Recipe class

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
 import yaml
 
+class Recipe:
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
+    def __repr__(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
 def openYAML(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
