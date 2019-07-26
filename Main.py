7c35e098e39a76d35f406a3613fa8b2b4bf5e002
AtkLordOverAll <darkninjabuni@gmail.com>
2019-07-26 14:59:25 +0100

    Add YAML loading from file

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
+import yaml
+
+def openYAML(path):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
+
 def main():
-    pass
+    recipes = openYAML("./Recipes.yaml")
 
 if __name__ == '__main__':
     main()
