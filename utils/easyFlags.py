0ba9029688cddc7d5799580999770a7f27d1f390
AtkLordOverAll <darkninjabuni@gmail.com>
2019-07-30 22:51:20 +0100

    Update easyFlags Now has better way of finding a help command, via a flag variable isHelpCommand Uses formatted strings as opposed to concatenation for better aesthetic

--- utils/easyFlags.py
+++ utils/easyFlags.py
@@ -2,10 +2,11 @@ import getopt
 from sys import exit as SYSEXIT
 
 class EasyFlag:
-    def __init__(self, short, action, long = None, needsArg = False):
+    def __init__(self, short, action, *, long = None, needsArg = False, isHelpCommand = False):
         self.short = short # single letter call e.g. h
         self.long = long # string calls e.g. help
         self.needsArg = needsArg # if flag needs arg e.g. --inputfile ./foo.txt
+        self.isHelpCommand = isHelpCommand
         self.action = action # function to call if flag is present and used correctly
 
 class EasyArgHandler:
@@ -14,24 +15,27 @@ class EasyArgHandler:
         self.getoptLong = []
         self.flagTuples = []
         self.flagDict = {}
+        self.helpFlag = None
         for flag in flags:
             self.getoptShort += flag.short
-            self.flagDict[("-" + flag.short)] = flag.action
+            self.flagDict[f"-{flag.short}"] = flag.action
             if flag.needsArg:
                 self.getoptShort += ":"
             if flag.long:
-                tempStr = "-" + flag.long
+                tempStr = f"--{flag.long}"
+                self.flagDict[tempStr] = flag.action
                 if flag.needsArg:
                     tempStr += "="
                 self.getoptLong.append(tempStr)
-                self.flagDict["--" + flag.long] = flag.action
+            if flag.isHelpCommand:
+                self.helpFlag = flag
     
     def handleArgs(self, args):
         try:
             optList, args = getopt.getopt(args, self.getoptShort, self.getoptLong)
         except getopt.GetoptError:
-            if "--help" in self.flagDict.keys():
-                print("Invalid args, use --help for help")
+            if self.helpFlag:
+                print(f"Invalid args, use -{self.helpFlag.short} or --{self.helpFlag.long} for help")
             else:
                 print("Invalid args")
             SYSEXIT(2)
